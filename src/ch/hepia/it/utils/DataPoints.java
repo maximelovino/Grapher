aa9656016f35429a77be5d9980a8ee660379a820
Lovino Maxime <maximelovino@gmail.com>
2016-10-30 14:57:56 +0100

    implemented DataPoints Class as teacher said, TODO: GUI

---
+++ src/ch/hepia/it/utils/DataPoints.java
@@ -0,0 +1,80 @@
+package ch.hepia.it.utils;
+
+import ch.hepia.it.geom2D.Point2D;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+
+public class DataPoints{
+	private String filePath;
+	private ArrayList<ArrayList<Point2D>> data;
+	private double minX = Double.MAX_VALUE;
+	private double maxX = Double.MIN_VALUE;
+	private double minY = Double.MAX_VALUE;
+	private double maxY = Double.MIN_VALUE;
+
+	public DataPoints (String filePath) {
+		this.filePath = filePath;
+		data = new ArrayList<>();
+		readData();
+	}
+
+	private void readData () {
+		try {
+			LineNumberReader reader = new LineNumberReader(new FileReader(filePath));
+			String line;
+
+
+			while ((line=reader.readLine())!=null){
+				ArrayList<Point2D> lineList = new ArrayList<>();
+				String[] lineArray = line.split(" ");
+				if (lineArray.length!=2 && lineArray.length!=4) throw new FileFormatException(reader.getLineNumber());
+
+				double x = 0;
+				double y = 0;
+
+				for (int i = 0; i < lineArray.length; i++) {
+					if (i%2 == 0){
+						x = Double.valueOf(lineArray[i]);
+						this.minX = Double.min(x,this.minX);
+						this.maxX = Double.max(x,this.maxX);
+					}else{
+						y = Double.valueOf(lineArray[i]);
+						this.minY = Double.min(y,this.minY);
+						this.maxY = Double.max(y,this.maxY);
+						lineList.add(new Point2D(x,y));
+					}
+				}
+
+				this.data.add(lineList);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public double getMinX () {
+		return minX;
+	}
+
+	public double getMaxX () {
+		return maxX;
+	}
+
+	public double getMinY () {
+		return minY;
+	}
+
+	public double getMaxY () {
+		return maxY;
+	}
+
+	public ArrayList<ArrayList<Point2D>> getData () {
+		return data;
+	}
+}
